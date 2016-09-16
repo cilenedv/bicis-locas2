47ebf585978bd6d639b2b3dd261fb2d9d0bfe9b6
cilenedv <cilenedelacruz@gmail.com>
2016-09-16 13:34:33 -0500

    Subiendo cambios y el readme

--- js/main.js
+++ js/main.js
@@ -1,4 +1,66 @@
 function validateForm(){
-	
+  /* Escribe tú código aquí */
+  var nombre = document.getElementById("name").value;
+  var apellido = document.getElementById("lastname").value;
+  var email = document.getElementById("input-email").value;
+  var contrasena = document.getElementById("input-password").value;
+  var lista= document.querySelector("select");
+
+   if( nombre == null || nombre.length == 0 || /^\s+$/.test(nombre) ) {
+    	var span = document.createElement("span");
+        var referencia= document.getElementById("name");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debe ingresar su nombre ");
+        span.appendChild(texto);
+         return span;
+   }
+   else{
+       var m = /^[a-zA-Z]*$/;
+       if(!nombre.search(m)) {
+              console.log("letra")
+              m=/[a-z]/g;}
+              if(!nombre.search(m)){
+                  alert("La primera letra debe empezar con maýuscula");
+              }
+         else{
+            alert ("Ingrese datos válidos (A-Z)");
+        }
+   
+  }
+  if( apellido == null || apellido.length == 0 || /^\s+$/.test(apellido) ) {
+      	var span = document.createElement("span");
+        var referencia= document.getElementById("lastname");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debe ingresar su apellido ");
+        span.appendChild(texto);
+         return span;
+  } else if(!(/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(email))) {
+    	var span = document.createElement("span");
+        var referencia= document.getElementById("input-email");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Verifique su email ");
+        span.appendChild(texto);
+         return span;
+  } else if( contrasena == null || contrasena.length <= 6 || contrasena == "password" || contrasena == 123456 || contrasena == 098754 || /^\s+$/.test(contrasena) ) {
+    	var span = document.createElement("span");
+        var referencia= document.getElementById("input-password");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("La contraseña debe de tener al menos 6 caracteres");
+        span.appendChild(texto);
+        return span;
+  } else if( lista.selectedIndex == null || lista.selectedIndex == 0 ) { 
+        var span = document.createElement("span");
+         var referencia= document.querySelector("select");
+        var padre=referencia.parentNode;
+        padre.insertBefore(span,referencia);
+        var texto = document.createTextNode("Debes de seleccionar almenos un tipo de bici");
+        span.appendChild(texto);
+        return span;
+    }
 }
+
 
---
+++ readme.md
@@ -0,0 +1,12 @@
+#Nos piden que realicemos el siguiente imagen
+
+![image](http://i65.tinypic.com/2e1r9rt.png {width=40px height=50px})
+
+- Lo que gregamos fue el siguiente codigo
+		var span = document.createElement("span"); --> Es para crear un elemeno span
+        var referencia= document.getElementById("name"); --> creamos una referencia despues de id "name" este caso es del nombre.
+        var padre=referencia.parentNode; -- > para que nos retorne al padre
+        padre.insertBefore(span,referencia); --> para insertar al padre
+        var texto = document.createTextNode("Debe ingresar su nombre");--> creamos la variable para guardar el texto que aparecera
+        span.appendChild(texto);--> el hijo
+        return span;-->retorna
